df8acc2e7438a65a87a71db340feec9427354d30
Devon Moubry <devon@moubry.com>
2017-03-22 15:23:26 -0500

    debugging

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-function renderHTML() {
-    $('body').append('<div class="deckOfCardsContainer"></div><div class="diceContainer"></div><div ="probabilitesContainer"></div>');
-}
-
-/******************************************************************************/
-function Card(suit, value) {
-
-    'use strict';
-    this.suit = suit;
-    this.value = value;
-
-}
-
-function Deck() {
-
-    'use strict'
-    this.deck = new Array();
-
-    this.makeDeck = makeDeck;
-
-}
-
-function makeDeck() {
-
-    var suits = new Array("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
-    var values = new Array("Hearts", "Spades", "Clubs", "Diamonds");
-    var deck = new Array(52);
-
-}
-
+// function renderHTML() {
+//     $('body').append('<div class="deckOfCardsContainer"></div><div class="diceContainer"></div><div ="probabilitesContainer"></div>');
+// }
+//
+// /******************************************************************************/
+// function Card(suit, value) {
+//
+//     'use strict';
+//     this.suit = suit;
+//     this.value = value;
+//
+// }
+//
+// function Deck() {
+//
+//     'use strict'
+//     this.deck = new Array();
+//
+//     this.makeDeck = makeDeck;
+//
+// }
+//
+// function makeDeck() {
+//
+//     var suits = new Array("2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace");
+//     var values = new Array("Hearts", "Spades", "Clubs", "Diamonds");
+//     var deck = new Array(52);
+//
+// }
+//
 /******************************************************************************/
 function Dice(value) {
 
@@ -45,32 +45,28 @@ var die = new Dice(6);
 
 /******************************************************************************/
 function getProbabilities() {
-  //return 'hell yes';
-  var die1 = die.roll(6);
-  var die2 = die.roll(6);
-  var sum = die1 + die2;
-  var probArray = [];
-  //return die1 + ',' + die2 + ',' + sum;
-  // var sumOf2;
-  // var sumOf3;
-  // var sumOf4;
-  // var sumOf5;
-  // var sumOf6;
-  // var sumOf7;
-  // var sumOf8;
-  // var sumOf9;
-  // var sumOf10;
-  // var sumOf11;
-  // var sumOf12;
-  //
-  for (var i = 0; i < 1000; i++) {
-    if (sum === 2) {
-      console.log('hotdog!');
-    } else {
-      console.log('boo!', sum);
-    }
-  }
+  console.log('hell yes');
+  // var die1 = die.roll(6);
+  // var die2 = die.roll(6);
+  // var sum = die1 + die2;
+  // //var probArray = [];
+  // return die1 + ',' + die2 + ',' + sum;
+  // //var sumOf2Count = 0;
+  // // var sumOf3;
+  // // var sumOf4;
+  // // var sumOf5;
+  // // var sumOf6;
+  // // var sumOf7;
+  // // var sumOf8;
+  // // var sumOf9;
+  // // var sumOf10;
+  // // var sumOf11;
+  // // var sumOf12;
+  // //
+  // for (var i = 0; i < 1000; i++) {
+  //   console.log(sum);
+  // }
 }
 
-var results = getProbabilities();
-console.log(results);
+// var results = getProbabilities();
+// console.log(results);
