5e5dda6d0d668f332ea0a418e89ea91b944c7020
Devon Moubry <devon@moubry.com>
2017-03-22 20:41:31 -0500

    DRY probability code

--- main.js
+++ main.js
@@ -48,48 +48,19 @@ var die = new Dice(6);
 
 /******************************************************************************/
 function getProbabilities() {
-    var countSum2 = 0;
-    var countSum3 = 0;
-    var countSum4 = 0;
-    var countSum5 = 0;
-    var countSum6 = 0;
-    var countSum7 = 0;
-    var countSum8 = 0;
-    var countSum9 = 0;
-    var countSum10 = 0;
-    var countSum11 = 0;
-    var countSum12 = 0;
+    var probArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     for (var i = 0; i < 1000; i++) {
+
         var die1 = die.roll(6);
         var die2 = die.roll(6);
         var sum = die1 + die2;
-        if (sum === 2) {
-            countSum2++;
-        } else if (sum === 3) {
-            countSum3++;
-        } else if (sum === 4) {
-            countSum4++;
-        } else if (sum === 5) {
-            countSum5++;
-        } else if (sum === 6) {
-            countSum6++;
-        } else if (sum === 7) {
-            countSum7++;
-        } else if (sum === 8) {
-            countSum8++;
-        } else if (sum === 9) {
-            countSum9++;
-        } else if (sum === 10) {
-            countSum10++;
-        } else if (sum === 11) {
-            countSum11++;
-        } else if (sum === 12) {
-            countSum12++;
-        }
+        probArray[sum-2]++;
+
     }
-    var probArray = [countSum2, countSum3, countSum4, countSum5, countSum6, countSum7, countSum8, countSum9, countSum10, countSum11, countSum12];
+
     return probArray;
+
 }
 
 var results = getProbabilities();
